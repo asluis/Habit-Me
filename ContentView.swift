45ce216168bc13ceb9cfb68d7f387575ecd6ba64
asluis <asluisinfo@gmail.com>
2021-01-07 20:26:41 -0800

    Add goals for project + started working on model

--- ContentView.swift
+++ ContentView.swift
@@ -5,12 +5,39 @@
 //  Created by Luis Alvarez on 1/7/21.
 //
 
+/*
+ My ideas are prefixed with a '-'
+ 
+ Part A:
+    At the very least, this means there should be a list of all activities they want to track, plus a form to add new activities – a title and description should be enough.
+ 
+    - Navigation View -> List
+    - List should display the user's score before they even click on it
+        - Similar to iExpense app UI
+    - Adding new activities will be done via sheet view
+ 
+Part B:
+ 
+    For a bigger challenge, tapping one of the activities should show a detail screen with the description, how many times they have completed it, plus a button incrementing their completion count.
+ 
+    - Make a custom shape that user can click on (not sure if possible)
+    - Inside custom shape will be the number of times they have checked in
+
+ Part C:
+    For an even bigger challenge, use Codable and UserDefaults to load and save all your data.
+    
+    - All data should be a strings and ints, so no need for special implementation
+ */
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Text("Hello, world!")
+                .padding()
+        }
+        
     }
 }
 
---
+++ Habit_Model.swift
@@ -0,0 +1,31 @@
+//
+//  Habit_Model.swift
+//  Habit Me
+//
+//  Created by Luis Alvarez on 1/7/21.
+//
+
+import Foundation
+
+
+class Habits: ObservableObject{
+    
+    @Published var list = [Habit]()
+    
+    
+}
+
+
+struct Habit: Codable, Identifiable{
+    var count = 0
+    var name = ""
+    var description:String? // User might not want a description
+    
+    let id = UUID() // Makes habit unique
+    
+    init(count:Int, name:String, desc:String){
+        self.count = count
+        self.name = name
+        self.description = desc
+    }
+}
