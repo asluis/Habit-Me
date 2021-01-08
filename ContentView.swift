0f793a28394fc3fab2804be3e19060f0df79c818
asluis <asluisinfo@gmail.com>
2021-01-07 20:05:56 -0800

    Initial code upload

---
+++ ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Habit Me
+//
+//  Created by Luis Alvarez on 1/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    
+}
---
+++ Habit_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_MeApp.swift
+//  Habit Me
+//
+//  Created by Luis Alvarez on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
