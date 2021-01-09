ad2a33b44e879bf4e7a8fcdcc3de2df2b163c4ae
asluis <asluisinfo@gmail.com>
2021-01-08 20:29:15 -0800

    Modified 'Add_Habit_View'

---
+++ Add_Habit_View.swift
@@ -0,0 +1,47 @@
+//
+//  Add_Habit_View.swift
+//  Habit Me
+//
+//  Created by Luis Alvarez on 1/8/21.
+//
+
+import SwiftUI
+
+struct Add_Habit_View: View {
+    
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var name = ""
+    @State var desc = ""
+    
+    @ObservedObject var habits:Habits
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section(header: Text("Details")){
+                    TextField("Habit Name", text: $name)
+                }
+                Section(header: Text("OPTIONAL")){
+                    TextField("What does this habit mean to you?", text: $desc)
+                }
+            }
+            .navigationBarTitle("Enter Habit Details")
+            .navigationBarItems(leading:
+                Button(action: {
+                    print("TODO")
+                }){
+                     Image(systemName: "arrowshape.turn.up.backward")
+                        .padding()
+                }
+            )
+        }
+    }
+}
+
+struct Add_Habit_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Add_Habit_View(habits: Habits())
+    }
+}
